882ad48b5ed419a43620d58451bd60bf9b79ffbb
vijay <vijay@vijay-PC>
2018-02-04 12:27:10 +0530

    Cheked code for serialization

---
+++ CoreConcepts/src/com/test/serialization/Student.java
@@ -0,0 +1,29 @@
+package com.test.serialization;
+
+public class Student {
+	
+	private int rollNO;
+	private String fName;
+	private String lNmae;
+	
+	
+	public int getRollNO() {
+		return rollNO;
+	}
+	public void setRollNO(int rollNO) {
+		this.rollNO = rollNO;
+	}
+	public String getfName() {
+		return fName;
+	}
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+	public String getlNmae() {
+		return lNmae;
+	}
+	public void setlNmae(String lNmae) {
+		this.lNmae = lNmae;
+	}
+	
+}
